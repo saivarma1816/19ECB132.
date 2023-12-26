686dffa30ba031b23170a7080487648687eff876
SurajAravind <63137467+SurajAravind@users.noreply.github.com>
2023-12-26 05:22:07 +0000

    Modified 01 program by adding time header file

--- 01_letter_count.c
+++ 01_letter_count.c
@@ -1,13 +1,19 @@
 # include <stdio.h>
 # include <string.h>
+# include <time.h>
 int main(void) {
   char sentence[20];
   int number_of_letters = 0;
   printf("Enter a sentence: ");
   scanf("%s", sentence);
+
+  time_t start, end;
+  start = clock();
   for (int i=0; i<strlen(sentence); i++)
     if (sentence[i] !=  ' ')
       number_of_letters++;
+  end = clock();
+  printf("Execution Time : %ld\n",(end-start));
   printf("\nThe number of letters in the given sentence are : %d\n", number_of_letters);
   return 0;
 }
